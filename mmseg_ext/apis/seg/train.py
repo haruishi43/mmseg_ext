936fc2462f5cd8712d8e7a2a5816b1b949374bfa
haruishi43 <toraudonn@gmail.com>
2022-12-01 02:17:59 +0000

    remvoe redundancy

--- mmseg_ext/apis/seg/train.py
+++ mmseg_ext/apis/seg/train.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 # Modified mmseg (Apache License)
 
-import random
 import warnings
 
-import numpy as np
 import torch
-import torch.distributed as dist
 
 import mmcv
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
-from mmcv.runner import build_optimizer, build_runner, get_dist_info
+from mmcv.runner import build_optimizer, build_runner
 
 from mmseg import digit_version
 from mmseg.core import DistEvalHook, EvalHook
@@ -19,56 +16,6 @@ from mmseg.utils import find_latest_checkpoint
 from mmseg_ext.datasets import build_dataloader, build_seg_dataset
 from mmseg_ext.core import EvalSegHook, DistEvalSegHook
 from mmseg_ext.utils import get_root_logger
-
-
-def init_random_seed(seed=None, device="cuda"):
-    """Initialize random seed.
-
-    If the seed is not set, the seed will be automatically randomized,
-    and then broadcast to all processes to prevent some potential bugs.
-    Args:
-        seed (int, Optional): The seed. Default to None.
-        device (str): The device where the seed will be put on.
-            Default to 'cuda'.
-    Returns:
-        int: Seed to be used.
-    """
-    if seed is not None:
-        return seed
-
-    # Make sure all ranks share the same random seed to prevent
-    # some potential bugs. Please refer to
-    # https://github.com/open-mmlab/mmdetection/issues/6339
-    rank, world_size = get_dist_info()
-    seed = np.random.randint(2**31)
-    if world_size == 1:
-        return seed
-
-    if rank == 0:
-        random_num = torch.tensor(seed, dtype=torch.int32, device=device)
-    else:
-        random_num = torch.tensor(0, dtype=torch.int32, device=device)
-    dist.broadcast(random_num, src=0)
-    return random_num.item()
-
-
-def set_random_seed(seed, deterministic=False):
-    """Set random seed.
-
-    Args:
-        seed (int): Seed to be used.
-        deterministic (bool): Whether to set the deterministic option for
-            CUDNN backend, i.e., set `torch.backends.cudnn.deterministic`
-            to True and `torch.backends.cudnn.benchmark` to False.
-            Default: False.
-    """
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    if deterministic:
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
 
 
 def train_seg_det(
